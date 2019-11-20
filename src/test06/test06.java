6d48449d4e26bd9298bc4c9dcb3c79f29ea2e102
DQing <DQing@192.168.43.31>
2019-11-20 17:53:42 +0800

    第一次提交

---
+++ src/test06/test06.java
@@ -0,0 +1,14 @@
+package test06;
+
+public class test06 {
+
+	public test06() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Œ“¿¥¡À");
+	}
+
+}
